55d5436eb9c4ad84fb345710feb931bf0df216b2
Adrian <adru75@gmail.com>
2023-01-12 15:45:21 -0300

    edu y expe

--- src/app/componentes/educacion/educacion.component.ts
+++ src/app/componentes/educacion/educacion.component.ts
@@ -1,18 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-import { PorfolioService } from 'src/app/servicios/porfolio.service';
+import { Educacion } from 'src/app/model/educacion';
+import { EducacionService } from 'src/app/servicios/educacion.service';
+import { TokenService } from 'src/app/servicios/token.service';
 @Component({
   selector: 'app-educacion',
   templateUrl: './educacion.component.html',
   styleUrls: ['./educacion.component.css']
 })
 export class EducacionComponent implements OnInit {
-  educacionList:any;
-  constructor(private datosPorfolio:PorfolioService) { }
+  educacion: Educacion[] = [];
+
+  constructor(private educacionS: EducacionService, private tokenService: TokenService) { }
+
+  isLogged = false;
 
   ngOnInit(): void {
-    this.datosPorfolio.obtenerDatos().subscribe(data => {
-      this.educacionList=data.education;
-    })
+    this.cargarEducacion();
+    if(this.tokenService.getToken()){
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
+    
   }
+
+  cargarEducacion(): void{
+    this.educacionS.lista().subscribe(
+      data=> {
+        this.educacion = data;
+      }
+    )
+  }
+
+  delete(id?: number) {
+    if(id != undefined) {
+      this.educacionS.delete(id).subscribe(
+        data => {
+          this.cargarEducacion();
+        }, err => {
+          alert("No se pudo eliminar.");
+        }
+      )
+    }
+  }
+
 
 }
